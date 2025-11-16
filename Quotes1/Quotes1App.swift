05200a4e2c9c506cc5dd6433dc7c42029e1c2050
justlele24 <lele.barra2403@gmail.com>
2025-11-16 19:22:22 +0100

    Initial Commit

---
+++ Quotes1/Quotes1App.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes1App.swift
+//  Quotes1
+//
+//  Created by Raffaele Barra on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Quotes1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
